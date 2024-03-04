4026d74e9fb62a322143407229fc6f17f91abbb3
crisuroll <cristina.cg9@hotmail.com>
2024-03-04 22:41:43 +0100

    Javadoc generado

---
+++ Simon_Dice_v3/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"main","l":"Engine"},{"p":"main","l":"Engine.tColores"},{"p":"main","l":"Engine.tModo"}];updateSearchResults();
